ec12786a1b8989c1350774c583f120e925d9ca7d
Nikitka <nik.nikitosa@mail.ru>
2025-02-11 22:33:11 +0300

    3 commit

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
+
 from .models import Post, Category
 
 
-def get_posts():
-    return Post.objects.select_related(
-        'category', 'location', 'author'
-    ).filter(
+def posts_filter(posts):
+    return posts.filter(
         pub_date__lte=timezone.now(),
         is_published=True,
         category__is_published=True
@@ -14,16 +13,26 @@ def get_posts():
 
 
 def index(request):
-    return render(request,
-                  'blog/index.html',
-                  {'post_list': get_posts()[0:5]})
+    return render(
+        request,
+        'blog/index.html',
+        {'post_list': posts_filter(
+            Post.objects.select_related(
+                'category', 'location', 'author'
+            )
+        )[:5]}
+    )
 
 
 def post_detail(request, post_id):
     return render(request,
                   'blog/detail.html',
                   {'post': get_object_or_404(
-                      get_posts(),
+                      posts_filter(
+                          Post.objects.select_related(
+                                'category', 'location', 'author'
+                          )
+                        ),
                       id=post_id
                   )
                   })
@@ -31,13 +40,14 @@ def post_detail(request, post_id):
 
 def category_posts(request, category_slug):
     category = get_object_or_404(
-        Category.objects.values(
-            'title', 'description'
-        ).filter(is_published=True),
+        Category.objects.all().filter(is_published=True),
         slug=category_slug
     )
     return render(request,
                   'blog/category.html',
                   {'category': category,
-                   'post_list': get_posts(
-                   ).filter(category__slug=category_slug)})
+                   'post_list': posts_filter(
+                       category.category_posts.select_related(
+                            'category', 'location', 'author'
+                       )
+                   )})
